ac0ad27ce5a4fc78ed9867208895cb89a3d3fac6
subtlemocha00 <kturley@gmail.com>
2024-02-06 16:32:20 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Board extends React.Component {
+	constructor() {
+		super(props);
+		this.state = {};
+	}
+}
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Tic-Tac-Toe</p>
+			</header>
+		</div>
+	);
+}
+
+export default App;
