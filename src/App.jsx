b2838835a677303d0865dae9871c8b6ed9504426
subtlemocha00 <kturley@gmail.com>
2024-02-07 01:13:21 -0500

    Board and Square Components

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,42 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Board extends React.Component {
-	constructor() {
+	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			squares: Array(9).fill(""),
+		};
+	}
+	componentDidMount() {}
+	handleClick = (square) => {
+		console.log(square);
+		// this.setState()
+	};
+	render() {
+		return (
+			<div>
+				{this.state.squares.forEach((square) => {
+					{
+						/* console.log(square); */
+					}
+					<Square handleClick={this.handleClick} status={square} />;
+				})}
+			</div>
+		);
+	}
+}
+
+class Square extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { props };
+	}
+	componentDidMount() {
+		console.log("mounted");
+		console.log(this.state);
+	}
+	render() {
+		return <button>{this.props.status}</button>;
 	}
 }
 
@@ -14,6 +47,7 @@ function App() {
 			<header className="App-header">
 				<p>Tic-Tac-Toe</p>
 			</header>
+			<Board />
 		</div>
 	);
 }
